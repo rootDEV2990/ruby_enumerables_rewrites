3a664afd7d967561ef7799d4c77e6a2d4d2c1d33
rootDEV2990 <website.dev@icloud.com>
2020-05-26 15:38:18 -0500

    Fixed method my_inject with symbol

--- myenumerables.rb
+++ myenumerables.rb
@@ -150,12 +150,10 @@ module Enumerable
       end
       sum
     elsif query.class == Symbol
-      total = ''.to_i
-      array.reduce(0) do |sum, num|
+        array = array.reduce(0) do |sum, num|
         sum = sum.public_send(query, num)
-        total = sum
       end
-      total
+      array
     else
       array.push(query)
       array
