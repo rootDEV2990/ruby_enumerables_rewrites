91a659ad9e69d6afe266392883113b64ccc9cee0
chindrop <apronticaleb@gmail.com>
2020-07-22 19:53:03 +0000

    It for test corrected

--- spec/myenumerables_spec.rb
+++ spec/myenumerables_spec.rb
@@ -12,7 +12,7 @@ describe Enumerable do
     end
 
     describe '.my_each_with_index' do
-        it 'Makes self into array unless block is given and adds index too' do
+        it 'Take an array and gets the element and index of the block_given?' do
             test1 = arr.my_each_with_index
             test2 = arr.each_with_index
             expect(test1 == test2)
@@ -20,7 +20,7 @@ describe Enumerable do
     end
 
     describe '.my_select' do
-        it 'Makes self into array unless block is given and adds index too' do
+        it 'Take an array and filters block_given' do
             test1 = arr.my_select { |item| item == 20}
             test2 = arr.select { |item| item == 20}
             expect(test1 == test2)
@@ -28,7 +28,7 @@ describe Enumerable do
     end
 
     describe '.my_any?' do
-        it 'Makes self into array unless block is given and adds index too' do
+        it 'Take an array and filters to find if an element is included to return true or false' do
             test1 = arr.my_any?(100)
             test2 = arr.any?(100)
             expect(test1 == test2)
@@ -36,7 +36,7 @@ describe Enumerable do
     end
 
     describe '.my_count' do
-        it 'Makes self into array unless block is given and adds index too' do
+        it 'Take an array and counts how many elements meet the criteria' do
             test1 = arr.my_count
             test2 = arr.count
             expect(test1 == test2)
@@ -44,7 +44,7 @@ describe Enumerable do
     end
 
     describe '.my_map' do
-        it 'Makes self into array unless block is given and adds index too' do
+        it 'Takes an array and transforms it.' do
             test1 = arr.my_map
             test2 = arr.map
             expect(test1 == test2)
@@ -52,7 +52,7 @@ describe Enumerable do
     end
 
     describe '.my_inject' do
-        it 'Makes self into array unless block is given and adds index too' do
+        it 'it will pass each element and accumulate each sequentially' do
             test1 = arr.my_inject(0) { |total, n| total + n }
             test2 = arr.inject(0) { |total, n| total + n }
             expect(test1 == test2)
