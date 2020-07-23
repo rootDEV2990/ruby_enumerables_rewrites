211c88d97e53dc0678a9a5ff0ceef90afc381f5d
rootDEV2990 <website.dev@icloud.com>
2020-07-23 15:19:03 -0500

    Added testing methods for block with each and each with index

--- spec/myenumerables_spec.rb
+++ spec/myenumerables_spec.rb
@@ -5,17 +5,27 @@ describe Enumerable do
 
     describe '.my_each' do
         it 'Makes self into array unless block is given' do
-            test1 = arr.my_each
-            test2 = arr.each
-            expect(test1 == test2)
+            expect(arr.my_each).to be_a(Enumerable)
+        end
+        it 'if block is given' do
+            result = []
+            arr.my_each { |item| result << item }
+            expect(arr).to eq(result)
         end
     end
 
     describe '.my_each_with_index' do
+        it 'If block is not given' do
+            expect(arr.my_each_with_index).to be_a(Enumerable)
+        end
         it 'Take an array and gets the element and index of the block_given?' do
-            test1 = arr.my_each_with_index
-            test2 = arr.each_with_index
-            expect(test1 == test2)
+            hash_my_method = Hash.new
+            hash_ruby_method = Hash.new
+            arr.my_each_with_index do |item, index|
+                hash_my_method[item] = index
+            end
+            arr.each_with_index { |item, index| hash_ruby_method[item] = index }
+            expect(hash_my_method).to eq(hash_ruby_method)
         end
     end
 
